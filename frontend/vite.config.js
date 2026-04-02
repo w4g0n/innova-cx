2201ebc24aa3c2e275450d16026969fe38cd3848
rami <ramitharwat05@gmail.com>
2026-04-02 23:04:45 +0400

    security changes, csp headers.strip debug info,

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,19 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: ['innovacx.net', 'www.innovacx.net', 'localhost', 'staff.innovacx.net'],
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['innovacx.net', 'www.innovacx.net', 'localhost', 'staff.innovacx.net'],
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      }
+    }
   }
 })
