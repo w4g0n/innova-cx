9675f5ca891de8fe1d1e84b385fb014ed281c0d5
Hamad Alaa <has071@uowmail.edu.au>
2026-04-12 20:32:33 +0400

    Fixed ui

--- frontend/src/components/common/KpiCard.jsx
+++ frontend/src/components/common/KpiCard.jsx
@@ -3,16 +3,9 @@ import "./KpiCard.css";
 export default function KpiCard({ label, value, caption }) {
   return (
     <div className="kpiCard">
-      <span className="kpiCard__glow" aria-hidden="true" />
-      <span className="kpiCard__spark" aria-hidden="true" />
-      <div className="kpiCard__inner">
-        <div className="kpiCard__meta">
-          <span className="kpiLabel">{label}</span>
-          <span className="kpiCard__status" aria-hidden="true" />
-        </div>
-        <span className="kpiValue">{value}</span>
-        {caption && <span className="kpiCaption">{caption}</span>}
-      </div>
+      <span className="kpiLabel">{label}</span>
+      <span className="kpiValue">{value}</span>
+      {caption && <span className="kpiCaption">{caption}</span>}
     </div>
   );
 }
